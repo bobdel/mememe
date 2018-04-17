18b6f06d78634ef9cdaa6944c343c00d0459b6f9
Bob DeLaurentis <lists@bobdel.com>
2018-04-17 08:37:31 -0700

    Info plist and Meme struct added.

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,18 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Robert DeLaurentis on 4/17/18.
+//  Copyright © 2018 Robert DeLaurentis. All rights reserved.
+//
+
+import UIKit
+
+struct Meme {
+
+  let topText: String
+  let bottomText: String
+  let memedImage: UIImage
+
+
+}
