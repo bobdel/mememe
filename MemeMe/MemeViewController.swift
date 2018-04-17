544037891419c96bd4e39c772088626a545a151b
Bob DeLaurentis <lists@bobdel.com>
2018-04-17 09:03:41 -0700

    Fixed Hide Status Bar. Added control in project settings and Info.plist.

--- MemeMe/MemeViewController.swift
+++ MemeMe/MemeViewController.swift
@@ -20,14 +20,13 @@ class MemeViewController: UIViewController {
   // MARK: User Target Action Methods
 
   // MARK: ViewController Lifecycle
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(true)
+  }
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
 
 
